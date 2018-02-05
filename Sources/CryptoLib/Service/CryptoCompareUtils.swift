9b3221c32542902bb8e9410344b978f472c7c754
Thomas Burguiere <thomas.burguiere@gmail.com>
2018-02-05 08:06:38 +0100

    wip cryptocompare websocket

---
+++ Sources/CryptoLib/Service/CryptoCompareUtils.swift
@@ -0,0 +1,89 @@
+//
+// Created by Thomas Burguiere on 01.02.18.
+//
+
+import Foundation
+
+let TRADE_FIELDS = [
+    "T": 0x0, // hex for binary 0, it is a special case of fields that are always there TYPE
+    "M": 0x0, // hex for binary 0, it is a special case of fields that are always there MARKET
+    "FSYM": 0x0, // hex for binary 0, it is a special case of fields that are always there FROM SYMBOL
+    "TSYM": 0x0, // hex for binary 0, it is a special case of fields that are always there TO SYMBOL
+    "F": 0x0, // hex for binary 0, it is a special case of fields that are always there FLAGS
+    "ID": 0x1, // hex for binary 1                                                       ID
+    "TS": 0x2, // hex for binary 10                                                      TIMESTAMP
+    "Q": 0x4, // hex for binary 100                                                     QUANTITY
+    "P": 0x8, // hex for binary 1000                                                    PRICE
+    "TOTAL": 0x10, // hex for binary 10000                                                   TOTAL
+];
+
+struct Field{
+    let name: String
+    let value: Int
+
+    init(_ name: String, _ value: Int) {
+        self.name = name
+        self.value = value
+    }
+}
+
+let CURRENT_FIELDS = [
+    "TYPE": 0x0       // hex for binary 0, it is a special case of fields that are always there
+    , "MARKET": 0x0       // hex for binary 0, it is a special case of fields that are always there
+    , "FROMSYMBOL": 0x0       // hex for binary 0, it is a special case of fields that are always there
+    , "TOSYMBOL": 0x0       // hex for binary 0, it is a special case of fields that are always there
+    , "FLAGS": 0x0       // hex for binary 0, it is a special case of fields that are always there
+    , "PRICE": 0x1       // hex for binary 1
+    , "BID": 0x2       // hex for binary 10
+    , "OFFER": 0x4       // hex for binary 100
+    , "LASTUPDATE": 0x8       // hex for binary 1000
+    , "AVG": 0x10      // hex for binary 10000
+    , "LASTVOLUME": 0x20      // hex for binary 100000
+    , "LASTVOLUMETO": 0x40      // hex for binary 1000000
+    , "LASTTRADEID": 0x80      // hex for binary 10000000
+    , "VOLUMEHOUR": 0x100     // hex for binary 100000000
+    , "VOLUMEHOURTO": 0x200     // hex for binary 1000000000
+    , "VOLUME24HOUR": 0x400     // hex for binary 10000000000
+    , "VOLUME24HOURTO": 0x800     // hex for binary 100000000000
+    , "OPENHOUR": 0x1000    // hex for binary 1000000000000
+    , "HIGHHOUR": 0x2000    // hex for binary 10000000000000
+    , "LOWHOUR": 0x4000    // hex for binary 100000000000000
+    , "OPEN24HOUR": 0x8000    // hex for binary 1000000000000000
+    , "HIGH24HOUR": 0x10000   // hex for binary 10000000000000000
+    , "LOW24HOUR": 0x20000   // hex for binary 100000000000000000
+    , "LASTMARKET": 0x40000   // hex for binary 1000000000000000000, this is a special case and will only appear on CCCAGG messages
+
+]
+
+struct CryptoCompareUtils {
+
+    static func unpackCurrent(tradeString: String) -> Dictionary<String, Any> {
+        var valuesArray: Array<Substring> = tradeString.split(separator: "~");
+        let mask = valuesArray.last!;
+        let maskInt = Int(mask, radix: 16);
+        var unpackedCurrent = Dictionary<String, Any>();
+        var currentFieldIndex = 0;
+
+        // TODO : manage to have CURRENT_FIELDS as an ordered dictionary
+        for property in CURRENT_FIELDS.keys {
+            if currentFieldIndex < valuesArray.count {
+
+                if CURRENT_FIELDS[property] == 0 {
+                    unpackedCurrent[property] = valuesArray[currentFieldIndex];
+                    currentFieldIndex += 1;
+                } else if (maskInt != nil && CURRENT_FIELDS[property] != nil) {
+                    //i know this is a hack, for cccagg, future code please don't hate me:(, i did this to avoid
+                    //subscribing to trades as well in order to show the last market
+                    if property == "LASTMARKET" {
+                        unpackedCurrent[property] = valuesArray[currentFieldIndex];
+                    } else {
+                        unpackedCurrent[property] = Float(valuesArray[currentFieldIndex]);
+                    }
+                    currentFieldIndex += 1;
+                }
+            }
+        }
+
+        return unpackedCurrent;
+    }
+}
---
+++ Tests/CryptoLibTests/Service/CryptoCompareUtilsTest.swift
@@ -0,0 +1,31 @@
+//
+// Created by Thomas Burguiere on 21.01.18.
+//
+
+import XCTest
+
+@testable import CryptoLib
+
+
+class CryptoCompareUtilsTest: XCTestCase {
+
+
+    private let response1 = "5~CCCAGG~BTC~USD~1~7297.99~1517844556~1.59258343~11659.940324402~187372512~151340.61056165057~1185162567.5865831~232723.41565274488~1855970075.362615~fe9"
+
+    private let response2 = "5~CCCAGG~BTC~USD~2~7297.94~1517844557~0.02239538~163.963295594~187372553~151351.85894286056~1185243952.2064326~232734.66403395488~1856051459.9824646~fe9"
+
+    private let response4 = "5~CCCAGG~BTC~USD~4~7297~1517844555~0.1288158~942.8028402000001~187372503~151337.52627822058~1185139983.8338788~232720.33136931487~1855947491.6099107~8217.6~8391.29~7221.14~8418.1~8600.04~7207.29~Bitfinex~7ffe9"
+
+    func test_update_data_is_unpacked_correctly() {
+        let updateResponse = "5~CCCAGG~BTC~USD~4~1517441113~0.00088366~8.8585942974~2905510661~119684.06513938117~1204555739.1975193~118320.2693874718~1190868665.3976107~10112.27~10398.87~9683.61~Gemini~78fe8"
+
+        let actual = CryptoCompareUtils.unpackCurrent(tradeString: updateResponse)
+        XCTAssertEqual(actual["TOSYMBOL"] as! Substring, "USD")
+        XCTAssertEqual(actual["FROMSYMBOL"] as! Substring, "BTC")
+
+        print(actual)
+
+    }
+
+
+}
